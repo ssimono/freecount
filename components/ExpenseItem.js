77c26af7582f9d73607dd8bbf282b1db30a3d8d5
Simon Alfassa <simon@sa-web.fr>
2019-12-29 15:42:23 +0100

    Move the expense list to grid display

--- components/ExpenseItem.js
+++ components/ExpenseItem.js
@@ -3,12 +3,10 @@ import {pretty} from './utils.js'
 export default function ExpenseItem (expense) {
   const li = document.createElement('LI')
   li.innerHTML = `
-    <div class="meta">
-      <data class="amount" value="${expense.amount}">${pretty(expense.amount)}</data><br>
-      <time date="${expense.date}">${(new Date(expense.date)).toDateString().slice(0, -5)}</time>
-    </div>
-    ${expense.title}</span><br>
+    <span class="title">${expense.title}</span>
+    <data class="amount" value="${expense.amount}">${pretty(expense.amount)}</data>
     <span class="creditor">paid by ${expense.creditor}</span>
+    <time date="${expense.date}">${(new Date(expense.date)).toDateString().slice(0, -5)}</time>
   `
 
   return li
