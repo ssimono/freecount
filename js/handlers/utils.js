86d7a6b6a4b746701a9b3dfa2d00f432a3b151f5
Vinay Pinto <eggman64@users.noreply.github.com>
2020-06-29 18:31:05 +0200

    fixes #7 - rounding of balances

--- js/handlers/utils.js
+++ js/handlers/utils.js
@@ -1,6 +1,5 @@
 export function pretty (amount, showSign = false) {
-  const cents = Math.round(amount * 100).toString().substr(-2)
-  const units = Math.trunc(amount).toString()
+  const rounded = (Math.round(amount * 100) / 100).toString()
   const sign = (showSign && amount > 0) ? '+' : ''
-  return `€${sign}${units}.${cents}`
+  return `€${sign}${rounded}`
 }
